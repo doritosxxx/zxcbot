37a5bc0fd454e9bd71d1bac1968df2af4fb41638
George Vavilov <35401860+doritosxxx@users.noreply.github.com>
2022-03-21 21:14:54 +0400

    removed error checking

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { config } from "dotenv";
 config();
 
-import { Collection, Guild } from "discord.js";
+import { Collection } from "discord.js";
 import { client, commands as commandsArray } from "./storage";
 import type { CommandBase } from "./class/CommandBase";
 import "./deploySlashCommands";
-import SilentReplyError from "./errors/SilentReplyError";
 import { silentReply } from "./functions";
 
 // Add commands.
@@ -19,11 +18,11 @@ for (const command of commandsArray) {
     }
 }
 
-client.once("ready", (client) => {
+client.once("ready", client => {
     console.log(`Ready! Logged in as ${client.user.tag}`);
 });
 
-client.on("interactionCreate", async (interaction) => {
+client.on("interactionCreate", async interaction => {
     if (!interaction.isCommand()) return;
 
     const command = commands.get(interaction.commandName);
@@ -32,16 +31,12 @@ client.on("interactionCreate", async (interaction) => {
     try {
         await command.execute(interaction);
     } catch (error) {
-        if (error instanceof SilentReplyError) {
-            return silentReply(interaction, error.message);
-        }
-
         console.error(error);
-		return silentReply(interaction, "There was an error while executing this command!")
+        return silentReply(interaction, "There was an error while executing this command!");
     }
 });
 
-client.on("messageCreate", async (msg) => {
+client.on("messageCreate", async msg => {
     if (msg.author.bot) return;
 
     if (/(da|да)$/.test(msg.content.toLowerCase())) {
